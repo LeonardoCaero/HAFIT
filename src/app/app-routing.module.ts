6c3944dc5352baf5be025bf6d2f3a5d4deee6b1d
LeoCaero <leonardo_caeroledezma@iescarlesvallbona.cat>
2023-03-23 20:10:00 +0100

    Grid fix products

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductListComponent } from './product-list/product-list.component';
+import { TestShopComponent } from './test-shop/test-shop.component';
 
 const routes: Routes = [
   { path: 'product-list', component: ProductListComponent },
+  {path: 'test', component: TestShopComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,15 +10,20 @@ import { ProductDataService } from './services/product-data.service';
 import { MatCardModule } from '@angular/material/card';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ProductCardComponent } from './product-card/product-card.component';
+import { TestShopComponent } from './test-shop/test-shop.component';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatIconModule} from '@angular/material/icon'; 
 
 @NgModule({
-  declarations: [AppComponent, NavBarComponent, ProductListComponent, ProductCardComponent],
+  declarations: [AppComponent, NavBarComponent, ProductListComponent, ProductCardComponent, TestShopComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    MatCardModule,
+    MatCardModule,  
+    MatGridListModule,
+    MatIconModule
   ],
   providers: [ProductDataService],
   bootstrap: [AppComponent],
