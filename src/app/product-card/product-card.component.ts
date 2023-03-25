5d82bc7331d981d85191380e426b2a647dc050ef
Leonardo Caero <leo.caero.ledezma@gmail.com>
2023-03-25 03:12:13 +0100

    Product list DONE + animations for M9 / Leo Part

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 import { environment } from 'src/environments/environment';
 import { IProduct } from '../interfaces/iproduct';
@@ -9,20 +9,34 @@ import { IProduct } from '../interfaces/iproduct';
   styleUrls: ['./product-card.component.scss'],
 })
 export class ProductCardComponent implements OnInit {
-  constructor(private sanitizer: DomSanitizer) {}
+  constructor(private sanitizer: DomSanitizer, private elementRef: ElementRef) {}
 
   ngOnInit(): void {
-    if (this.product.image != null || this.product.image != '') {
+    if (this.product.image != null && this.product.image != '') {
       this.productImage = this.sanitizer.bypassSecurityTrustResourceUrl(
         `${this.product.image}`
       );
     } else {
-      this.product.image = environment.defaultImage;
+      this.productImage = environment.defaultImage;
     }
+
+    const buyButton = this.elementRef.nativeElement.querySelector('.buy');
+    const removeButton = this.elementRef.nativeElement.querySelector('.remove');
+    const bottomDiv = this.elementRef.nativeElement.querySelector('.bottom');
+
+    buyButton?.addEventListener('click', () => {
+      bottomDiv?.classList.add('clicked');
+    });
+
+    removeButton?.addEventListener('click', () => {
+      bottomDiv?.classList.remove('clicked');
+    });
   }
 
   @Input()
   product!: IProduct;
 
   productImage: any;
+
+  @ViewChild('bottomDiv') bottomDivRef!: ElementRef;
 }
