c8ca94c4f4e07e1e99d56536e370bf75042d25e7
LeoCaero <leonardo_caeroledezma@iescarlesvallbona.cat>
2023-03-30 19:34:38 +0200

    LOGIN

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,33 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.scss']
+  styleUrls: ['./nav-bar.component.scss'],
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit {
+  public isAuthenticated: boolean = false;
   public userProfile: any;
 
-  constructor(private auth: AuthService){
+  constructor(private auth: AuthService) {}
+
+  ngOnInit() {
+    this.auth.isAuthenticated$.subscribe((isAuthenticated) => {
+      this.isAuthenticated = isAuthenticated;
+    });
+
     this.auth.user$.subscribe((user) => {
       this.userProfile = user;
     });
 
+    this.auth.getAccessTokenSilently().subscribe((accessToken) => {
+      // Si se puede obtener un nuevo token de acceso, el usuario está autenticado.
+      this.isAuthenticated = true;
+    }, (error) => {
+      // Si no se puede obtener un nuevo token de acceso, el usuario no está autenticado.
+      this.isAuthenticated = false;
+      this.userProfile = null;
+    });
   }
-
-  playSound() {
-    let audio = new Audio();
-    audio.src = "../assets/sounds/swoosh.mp3";
-    audio.load();
-    audio.play();
-  }
-  login() {
-    this.auth.loginWithRedirect();
-  }
-
-  ngOnInit(): void{
-
-  }
-
 }
