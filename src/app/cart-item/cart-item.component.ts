5cdff77f71da8cf5eeaa9e6e824005c86bb647d7
LeoCaero <leonardo_caeroledezma@iescarlesvallbona.cat>
2023-04-26 20:59:21 +0200

    Genie update

--- src/app/cart-item/cart-item.component.ts
+++ src/app/cart-item/cart-item.component.ts
@@ -1,9 +1,10 @@
-import { Component, ElementRef, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { IProduct } from '../interfaces/iproduct';
 import { AuthServiceService } from '../services/auth-service.service';
 import { NavbarService } from '../services/navbar.service';
 import { UserDataService } from '../services/user-data.service';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-cart-item',
@@ -11,69 +12,70 @@ import { UserDataService } from '../services/user-data.service';
   styleUrls: ['./cart-item.component.scss'],
 })
 export class CartItemComponent implements OnInit {
+  @ViewChild('addButton') addButton!: ElementRef | undefined;
+  @ViewChild('remButton') remButton!: ElementRef | undefined;
+
   constructor(
-    private elementRef: ElementRef,
     private authService: AuthServiceService,
     private userService: UserDataService,
     private navbarService: NavbarService
   ) {}
 
   ngOnInit(): void {
-    const addButton = this.elementRef.nativeElement.querySelector('.addCart');
-    const remButton = this.elementRef.nativeElement.querySelector('.remCart');
-    if (this.product.image != null && this.product.image != '') {
-    } else {
+    if (this.product.image == null || this.product.image === '') {
       this.product.image = environment.defaultImage;
     }
 
-    addButton?.addEventListener('click', () => {
-      this.authService.checkUser().subscribe(
-        (response) => {
-          this.userService
-            .updateCart(response.userId, this.product.productId, 'add')
-            .subscribe({
-              next: (data) => {
-                data.body.cartItems.forEach((elem: IProduct) => {
-                  if ((elem.productId = this.product.productId)) {
-                    this.productPrice =
-                      parseFloat(elem.price.$numberDecimal) * elem.quantity;
-                  }
-                });
-              },
-            });
-        },
-        (error) => {
-          console.log('Error obteniendo userId');
-        }
-      );
-    });
-    remButton?.addEventListener('click', () => {
-      this.authService.checkUser().subscribe(
-        (response) => {
-          this.userService
-            .updateCart(response.userId, this.product.productId, 'remove')
-            .subscribe({
-              next: (data) => {
-                data.body.cartItems.forEach((elem: IProduct) => {
-                  if ((elem.productId = this.product.productId)) {
-                    this.productPrice =
-                      parseFloat(elem.price.$numberDecimal) * elem.quantity;
-                  }
-                });
-              },
-            });
-        },
-        (error) => {
-          console.log('Error obteniendo userId');
-        }
-      );
-    });
-    this.productPrice =
-      this.product.quantity * parseFloat(this.product.price.$numberDecimal);
+    const addClick$ = this.addButton?.nativeElement.click$;
+    const remClick$ = this.remButton?.nativeElement.click$;
+
+    addClick$
+      .pipe(
+        switchMap(() => this.authService.checkUser()),
+        switchMap((response: any) =>
+          this.userService.updateCart(
+            response.userId,
+            this.product.productId,
+            'add'
+          )
+        )
+      )
+      .subscribe((data: any) => {
+        data.body.cartItems.forEach((elem: IProduct) => {
+          if (elem.productId === this.product.productId) {
+            this.productPrice = (
+              parseFloat(elem.price.$numberDecimal) * elem.quantity
+            ).toFixed(2);
+          }
+        });
+      });
+
+    remClick$
+      .pipe(
+        switchMap(() => this.authService.checkUser()),
+        switchMap((response: any) =>
+          this.userService.updateCart(
+            response.userId,
+            this.product.productId,
+            'remove'
+          )
+        )
+      )
+      .subscribe((data: any) => {
+        data.body.cartItems.forEach((elem: IProduct) => {
+          if (elem.productId === this.product.productId) {
+            this.productPrice = (
+              parseFloat(elem.price.$numberDecimal) * elem.quantity
+            ).toFixed(2);
+          }
+        });
+      });
+
+    this.productPrice = (
+      this.product.quantity * parseFloat(this.product.price.$numberDecimal)
+    ).toFixed(2);
   }
 
-  @Input()
-  product!: IProduct;
-
-  productPrice!: Number;
+  @Input() product!: IProduct;
+  productPrice!: string;
 }
