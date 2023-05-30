4b0a285fe5fa6b213d7dac9a92ddce2685bc9291
Leonardo <leo.caero.ledezma@gmail.com>
2023-05-30 17:13:47 +0200

    Changes

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,9 +1,8 @@
-import { Component, NgModule, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService, User } from '@auth0/auth0-angular';
 import { UserDataService } from '../services/user-data.service';
 import { IUser } from '../interfaces/iuser';
 import { AuthServiceService } from '../services/auth-service.service';
-import { FormsModule } from '@angular/forms';
 import { NgDompurifySanitizer } from '@tinkoff/ng-dompurify';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import { ChangeEvent } from '@ckeditor/ckeditor5-angular';
@@ -18,6 +17,14 @@ import { environment } from 'src/environments/environment';
 export class UserProfileComponent implements OnInit {
   public userProfile: any;
   model: any;
+  fileName: any;
+  formdata: FormData = new FormData();
+  userData!: IUser;
+  bioData: string = '';
+  userName: string = '';
+  public Editor = ClassicEditor;
+  editActive = false;
+
   constructor(
     public auth: AuthService,
     private userService: UserDataService,
@@ -52,14 +59,6 @@ export class UserProfileComponent implements OnInit {
     });
   }
 
-  fileName: any;
-  formdata: FormData = new FormData();
-  userData!: IUser;
-  bioData: string = '';
-  userName: string = '';
-  public Editor = ClassicEditor;
-  editActive = false;
-
   onFileSelected(event: any) {
     const file: File = event.target.files[0];
     if (file) {
@@ -71,8 +70,7 @@ export class UserProfileComponent implements OnInit {
   }
 
   public onChange({ editor }: ChangeEvent) {
-    const data = editor.data.get();
-    this.bioData = data;
+    this.bioData = editor.data.get();
   }
 
   public nameChange(data: string) {
@@ -80,17 +78,20 @@ export class UserProfileComponent implements OnInit {
   }
 
   public onSaveBio() {
-    if (this.bioData != '') {
+    if (this.bioData && this.bioData !== '') {
       this.userData.biography = this.bioData;
     }
-    if (this.userName != '') {
+    if (this.userName && this.userName !== '') {
       this.userData.name = this.userName;
     }
-
+  
     this.userService.updateUser(this.userData).subscribe((resp) => {
       console.log(resp);
+      // Actualizar userData con los datos actualizados en la respuesta
+      this.userData = resp.body;
     });
   }
+  
 
   public profileEdit() {
     if (this.editActive) {
